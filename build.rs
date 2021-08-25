a3c32ac325f7e1c2c7c055c55430cdc1f17fc71d
Volker Mische <volker.mische@gmail.com>
2021-08-25 15:50:20 +0200

    fix: build CUDA kernel via ff-cl-gen
    Don't include a full CUDA kernel as source file, but generate it at
    build time with ff-cl-gen.

--- build.rs
+++ build.rs
@@ -2,48 +2,72 @@
 
 #[cfg(all(feature = "gpu", feature = "cuda"))]
 fn main() {
-    use std::env;
     use std::path::PathBuf;
     use std::process::Command;
+    use std::{env, fs};
+
+    use blstrs::Bls12;
+    use sha2::{Digest, Sha256};
 
     // Somehow the compiler thinks this module contains dead code
     #[allow(dead_code)]
     #[path = "src/gpu/sources.rs"]
     mod sources;
 
-    static CUDA_MULTIEXP_PATH: &str = "src/gpu/multiexp/multiexp32_ffclgen.cu";
-
-    // The kernel only needs to be re-compiled if it changed.
-    println!("cargo:rerun-if-changed={}", CUDA_MULTIEXP_PATH);
+    let kernel_source = sources::kernel::<Bls12>();
 
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR was not set.");
-    let fatbin_path: PathBuf = [&out_dir, "multiexp32_ffclgen.fatbin"].iter().collect();
 
-    // nvcc --optimize=6 --fatbin --gpu-architecture=sm_86 --generate-code=arch=compute_86,code=sm_86 --generate-code=arch=compute_80,code=sm_80 --generate-code=arch=compute_75,code=sm_75 --define-macro=BLSTRS --output-file multiexp32.fatbin src/gpu/multiexp/multiexp32.cu
-    let status = Command::new("nvcc")
-        .arg("--optimize=6")
+    // nvcc --optimize=6 --fatbin --gpu-architecture=sm_86 --generate-code=arch=compute_86,code=sm_86 --generate-code=arch=compute_80,code=sm_80 --generate-code=arch=compute_75,code=sm_75 -output-file multiexp32.fatbin src/gpu/multiexp/multiexp32.cu
+    let mut nvcc = Command::new("nvcc");
+    nvcc.arg("--optimize=6")
         .arg("--fatbin")
         .arg("--gpu-architecture=sm_86")
         .arg("--generate-code=arch=compute_86,code=sm_86")
         .arg("--generate-code=arch=compute_80,code=sm_80")
-        .arg("--generate-code=arch=compute_75,code=sm_75")
-        //.arg("--define-macro=BLSTRS")
-        .arg("--output-file")
-        .arg(&fatbin_path)
-        .arg(CUDA_MULTIEXP_PATH)
-        .status()
-        .expect("Cannot run nvcc.");
+        .arg("--generate-code=arch=compute_75,code=sm_75");
 
-    if status.success() {
-        // The idea to put the path to the farbin into a compile-time env variable is from
-        // https://github.com/LutzCle/fast-interconnects-demo/blob/b80ea8e04825167f486ab8ac1b5d67cf7dd51d2c/rust-demo/build.rs
-        println!(
-            "cargo:rustc-env=CUDA_MULTIEXP_FATBIN={}",
-            fatbin_path.to_str().unwrap()
-        );
-    } else {
-        panic!("nvcc failed.");
+    // Hash the source and and the compile flags. Use that as the filename, so that the kernel is
+    // only rebuilt if any of them change.
+    let mut hasher = Sha256::new();
+    hasher.update(kernel_source.as_bytes());
+    hasher.update(&format!("{:?}", &nvcc));
+    let kernel_digest = hex::encode(hasher.finalize());
+
+    let source_path: PathBuf = [&out_dir, &format!("{}.cu", &kernel_digest)]
+        .iter()
+        .collect();
+    let fatbin_path: PathBuf = [&out_dir, &format!("{}.fatbin", &kernel_digest)]
+        .iter()
+        .collect();
+
+    fs::write(&source_path, &kernel_source).expect(&format!(
+        "Cannot write kernel source at {}.",
+        source_path.to_str().unwrap()
+    ));
+
+    if !fatbin_path.as_path().exists() {
+        let status = nvcc
+            .arg("--output-file")
+            .arg(&fatbin_path)
+            .arg(&source_path)
+            .status()
+            .expect("Cannot run nvcc.");
+
+        if !status.success() {
+            panic!("nvcc failed.");
+        }
     }
+
+    // Make sure that build.rs is run if the compiled output (the fatbin) was deleted.
+    println!("cargo:rerun-if-changed={}", fatbin_path.to_str().unwrap());
+
+    // The idea to put the path to the farbin into a compile-time env variable is from
+    // https://github.com/LutzCle/fast-interconnects-demo/blob/b80ea8e04825167f486ab8ac1b5d67cf7dd51d2c/rust-demo/build.rs
+    println!(
+        "cargo:rustc-env=CUDA_MULTIEXP_FATBIN={}",
+        fatbin_path.to_str().unwrap()
+    );
 }
 
 #[cfg(not(all(feature = "gpu", feature = "cuda")))]
